1f21ce3ef8179cf6167942d04110146bf3cb7b62
Robin Gruenke <eimfach@me.com>
2020-03-09 20:41:22 +0100

    release

--- helpers/skeleton.py
+++ helpers/skeleton.py
@@ -43,6 +43,10 @@ def htmldocument(data):
 
         with doc.tag('section', klass='projects'):
           content(doc, data)
+        with doc.tag('div', klass='center'):
+          with doc.tag('a', href='', title='robingruenke.com'):
+            with doc.tag('span', klass='icon-home-house-streamline colorful-font font-medium'):
+              doc.text('')
 
         with doc.tag('div', klass='center'):
           doc.line('small', 'Copyright ' + str(data['year']) +  '-' + str(datetime.datetime.now().year) + ' Robin T. Gruenke')
