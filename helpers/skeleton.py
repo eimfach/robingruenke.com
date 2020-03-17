15ceb7c45c33715db0e0b5917f1c0d30db242766
Robin Gruenke <eimfach@me.com>
2020-03-17 09:55:52 +0100

    release

--- helpers/skeleton.py
+++ helpers/skeleton.py
@@ -76,6 +76,10 @@ def journalcontent(doc, data, enablefeedback=False, enablejournallike=False):
   for chapter in data['chapters']:
     helpers.components.chapter(doc, enablefeedback=enablefeedback, id=getnormalizedtopic(chapter['topic']), heading=chapter['topic'], datum=chapter['date'], paragraphs=chapter['paragraphs'], author=chapter['author'], picture=chapter.get('picture', None), appndx=chapter.get('appendix', None), gallery=chapter.get('gallery', None), quote=chapter.get('quote', None))
 
+  if len(data['chapters']) < 3:
+    with doc.tag('blockquote', klass='last no-border', id='more-info'):
+      doc.text('Note: Wonder where the rest of the article is ? In my Journal articles, I write and publish small chapters. Every now and then I add a new chapter. Just come back later !')
+
   if enablejournallike:
     helpers.components.like(doc, data['topic'])
 
