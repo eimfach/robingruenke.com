4eefa4259517937ccc1bbc9a310fe2f83c935561
Robin Gruenke <eimfach@me.com>
2021-08-08 17:24:59 +0200

    build script errors

--- build/build.sh
+++ build/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 pip install pipenv || exit 1
 
-rm -rf ../stylesheets/inline/critical || exit 2
-mkdir ../stylesheets/inline/critical || exit 3
+rm -rf ../stylesheets/inline/critical
+mkdir ../stylesheets/inline/critical
 
-pipenv install || exit 4
+pipenv install || exit 2
 
-pipenv run python -m pytest -vv || exit 5
-pipenv run python compile.py || exit 6
+pipenv run python -m pytest -vv || exit 3
+pipenv run python compile.py || exit 4
 
-node build-critical-css.js || exit 7
+node build-critical-css.js || exit 5
 
 echo '------> Rerun .journal complilation to include inline css'
-pipenv run python compile.py || exit 8
+pipenv run python compile.py || exit 6
